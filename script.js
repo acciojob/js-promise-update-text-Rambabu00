4536bee83d86a83d24a51cd565b2db64470e97ee
Rambabu00 <123948696+Rambabu00@users.noreply.github.com>
2023-03-25 09:46:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ function add(){
+	return "hello, world!";
+}
+async function ff(){
+	var prom=await add();
+document.getElementById("output").innerHTML=prom;
+	
+}
+ff();
